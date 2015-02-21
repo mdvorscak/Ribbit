d58d79f75769274b97bf7dcf31aa27ab118b1702
Mike Dvorscak <mike.dvorscak@gmail.com>
2015-02-21 17:19:14 -0600

    Finished users

---
+++ app/src/main/java/mikedvorscak/com/ribbit/EditFriendsActivity.java
@@ -0,0 +1,140 @@
+package mikedvorscak.com.ribbit;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.List;
+
+
+public class EditFriendsActivity extends ListActivity {
+
+    public static final String TAG = EditFriendsActivity.class.getSimpleName();
+
+    protected List<ParseUser> mUsers;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseUser mCurrentUser;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_edit_friends);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+
+        setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.orderByAscending(ParseConstants.KEY_USERNAME);
+        query.setLimit(1000);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            Context context = EditFriendsActivity.this;
+
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                setProgressBarIndeterminateVisibility(false);
+
+                if(e == null){
+                    mUsers = users;
+                    String[] usernames = new String[mUsers.size()];
+                    int i = 0;
+                    for(ParseUser user: mUsers){
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(context,
+                            android.R.layout.simple_list_item_checked, usernames);
+                    setListAdapter(adapter);
+                    
+                    addFriendsCheckmarks();
+                } else {
+                    Log.e(TAG, e.getMessage());
+                    Utils.displayErrorDialog(e.getMessage(),
+                            getString(R.string.error_dialog_title),
+                            context);
+                }
+            }
+        });
+    }
+
+    private void addFriendsCheckmarks() {
+        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> friends, ParseException e) {
+                if(e == null){
+                    for(int i = 0; i < mUsers.size(); i++){
+                        ParseUser user = mUsers.get(i);
+
+                        for(ParseUser friend: friends){
+                            if(friend.getObjectId().equals(user.getObjectId())){
+                                getListView().setItemChecked(i, true);
+                            }
+                        }
+                    }
+                } else {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        ParseUser userClicked = mUsers.get(position);
+        if(getListView().isItemChecked(position)) {
+            //Add friend
+            mFriendsRelation.add(userClicked);
+        } else {
+            //remove friend
+            mFriendsRelation.remove(userClicked);
+        }
+        mCurrentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/mikedvorscak/com/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package mikedvorscak.com.ribbit;
+
+/**
+ * Created by mike on 2/21/15.
+ */
+public final class ParseConstants {
+    //Class names
+
+    //Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
